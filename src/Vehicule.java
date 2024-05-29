65091a6607207296d6fe4301463583fdb61c6b1e
jad <kbj.jad@gmail.com>
2024-05-29 14:32:40 +0200

    Initialisation de la classe Vehicule

---
+++ src/Vehicule.java
@@ -0,0 +1,22 @@
+public class Vehicule {
+    private String numeroImmatriculation;
+
+    public Vehicule(String numeroImmatriculation) {
+        this.numeroImmatriculation = numeroImmatriculation;
+    }
+
+    public String getNumeroImmatriculation() {
+        return numeroImmatriculation;
+    }
+
+    public void setNumeroImmatriculation(String numeroImmatriculation) {
+        this.numeroImmatriculation = numeroImmatriculation;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicule{" +
+                "numeroImmatriculation='" + numeroImmatriculation + '\'' +
+                '}';
+    }
+}
